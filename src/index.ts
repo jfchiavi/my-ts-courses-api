a4b8e463e89dd10f5810f4189b502944eef2dd3a
jfchiavi <eljavi379@gmail.com>
2025-05-20 02:17:28 -0300

    feat: update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-console.log(
-hell
-world)
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.listen(6505, () => {
+    console.log("Server is running on port 6505");
+});
